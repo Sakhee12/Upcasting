ecbef5cfa243a46ba15b7ee5ffc8e5d3f97d7c3e
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-11 21:50:41 +0530

    Add files via upload

---
+++ Upcasting.java
@@ -0,0 +1,12 @@
+package PrinciplesOfOops;
+
+public class Upcasting 
+{
+	  public static void main(String[] args)
+	  {
+		  Father f1 = new Son();
+		  f1.drinking();
+		//  f1.smoking();
+	  }
+}
+
