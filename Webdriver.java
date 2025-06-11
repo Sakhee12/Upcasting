4eaec34e5f1cc041ab3a33279b3d8924f5b72937
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-11 21:51:41 +0530

    Add files via upload

---
+++ Webdriver.java
@@ -0,0 +1,26 @@
+package PrinciplesOfOops;
+
+public class Webdriver
+{
+
+	public void properties()
+	{
+		System.out.println("Properties of Webdriver:");
+	}
+	
+	public void ConfigurationManagement()
+	{
+		System.out.println("They provide a centralized place to store and manage test configuration parameters, separating them from the actual test code. ");
+	}
+	
+	public void Flexibility()
+	{
+		System.out.println("Allows for easy modification of test settings without altering the test scripts, enabling tests to be run in different environments with minimal changes. ");
+	}
+	
+	public void DynamicLoading()
+	{
+		System.out.println("Selenium WebDriver can load these properties dynamically, enabling the test scripts to adapt to different environments during execution. ");
+	}
+
+}
