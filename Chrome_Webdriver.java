acfae30ffb9192f39ae9a8dc26fa38851f968b97
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-11 21:52:31 +0530

    Add files via upload

---
+++ Chrome_Webdriver.java
@@ -0,0 +1,25 @@
+package PrinciplesOfOops;
+
+public class Chrome_Webdriver extends Webdriver
+{
+	public void ChromeProperties()
+	{
+		System.out.println("Properties of Chrome");
+	}
+	
+	public void Permission()
+	{
+		System.out.println("Camera On/Off");
+	}
+	
+	public void LogFileFeature()
+	{
+		System.out.println("Append log");
+		System.out.println("Readable Timestamps");
+	}
+	
+	public void Extension()
+	{
+		System.out.println("The extensions parameter accepts crx files. As for unpacked directories, please use the load-extension argument instead");
+	}
+}
